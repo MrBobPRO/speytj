1f9e126989e980b5a4f0dd1f9bf41d48f305a559
= <=>
2022-10-18 15:42:21 +0500

    Videos styles redisigned

--- resources/views/components/video-card.blade.php
+++ resources/views/components/video-card.blade.php
@@ -4,7 +4,7 @@
   <video class="plyr" height="220" src="{{ asset('videos/' . $video->filename) }}" playsinline controls></video>
 
   <div class="video-card__body">
-    <span class="video-card__date">{{ Carbon\Carbon::create($video->created_at)->locale('ru')->isoFormat('DD.MM.YYYY') }}</span>
     <h3 class="video-card__title">{{ $video->title }}</h3>
+    <span class="video-card__date">{{ Carbon\Carbon::create($video->created_at)->locale('ru')->isoFormat('DD.MM.YYYY') }}</span>
   </div>
 </div>
