84ff73998f83cf0488c55b4c8b5c4de2ad7a68a6
= <=>
2022-12-03 13:30:16 +0500

    Noindex removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <meta name="robots" content="none" />
-  <meta name="googlebot" content="noindex, nofollow" />
-  <meta name="yandex" content="none" />
-
   <link rel="manifest" href="{{ asset('manifest.json') }}">
   <meta name="msapplication-config" content="{{ asset('msapplication-config.xml') }}">
 
